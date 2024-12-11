99afc72954e359e2132aac0313aeeab9fcda6195
Haddoun1 <naoufal.haddouzi@hva.nl>
2024-12-11 18:12:58 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,13 @@
 // JavaScript Document
 console.log("Bomboclat");
 
-const menuBtn = document.querySelector("header nav > button") // roept de 2 buttons aan in nav
-let navMenu = document.querySelector("main > nav") // het vak wat nu buiten beeld is - zwarte menu
+const menuBtn = document.querySelector("header nav button")
+let menu = document.querySelector("main nav")
+const bodyEl = document.body 
 
-const likeBtns = document.querySelectorAll('main section:nth-of-type(5) ul li button')
-
-const profileIcon = document.querySelector('header nav ul li button') // profiel icoontje
-
-const popUp = document.querySelector('body > section') // kruisje in formulier
-const closePopUp = document.querySelector('body > section div button') // kruisje in formulier sluiten
-
-
-
-menuBtn.addEventListener("click", openMenu ) // hamburger menu openen (zwarte nav)
 function openMenu(){
-    navMenu.classList.toggle("active") // zwarte menu balk zichtbaar en dan verdwijnt
-    menuBtn.classList.toggle("active") // zorgt dat span daadwerkelijk de animatie uitvoert
+    menu.classList.toggle("active")
+    bodyEl.classList.toggle('active')
 }
+
+menuBtn.addEventListener("click", openMenu)
